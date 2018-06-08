17e819fcf3adbcd752d2d29bed0e03f40226a04c
Devin Mounts <devinmounts@gmail.com>
2018-06-08 12:13:50 -0700

    add array to hold user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 function beepboop(number){
-var response = [];
+  var response = [];
+  var array = [number];
+  console.log(array);
 
-  if (isNaN(number)) {
-    response.push("not a number");
-  } else if (number === 0) {
-    response.push("Beep")
-  } else if (number ===1) {
-    response.push("Boop")
-  } else if (number % 3 === 0) {
-    response.push("I'm sorry Dave, I can't allow you to do that.")
-  } else
-    response.push(number)
+    if (isNaN(array)) {
+        response.push("not a number");
+      } else if (array % 3 === 0) {
+        response.push("I'm sorry Dave, I can't allow you to do that.")
+      } else if (array === 1) {
+        response.push("Boop")
+      } else if (array === 0) {
+          response.push("Beep")
+      } else
+        response.push(array)
 
-      console.log(number)
+          console.log(array)
 
-  return response.join("");
+    return response.join("");
 }
