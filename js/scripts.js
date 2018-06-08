66b335d26140fd9cb4ade7f5ddd48f30f17af1c2
Devin Mounts <devinmounts@gmail.com>
2018-06-08 09:38:45 -0700

    create else statement to return number if entry is number.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,9 @@ var response = []
 
   if (isNaN(number)) {
     response.push("This is not a number.");
-  }
-  response.push(number)
+  } else {
+  response.push(number);
+    }
 
   console.log(number)
 return response.join("");
