2a8487bd25dcab3cada5290e30d9e2ae895c2461
Devin Mounts <devinmounts@gmail.com>
2018-06-08 15:49:06 -0700

    add UI with JQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function beepboop(number){
+function beepBoop(number){
   var response = [];
 
   for(i=0; i<=number; i++) {
-    response.push(i);
+    console.log(response);
     if (isNaN(i)) {
       response.push("not a number");
     } else if (i % 3 === 0) {
@@ -12,10 +12,18 @@ function beepboop(number){
       } else if (i === 0) {
           response.push("Beep")
       } else
-        response.push(i)
+          response.push(i)
 
           console.log(response);
-
   }
-    return response.join("");
+    return response.join(" ");
 }
+
+$(function(){
+  $("#beepboop").submit(function(e){
+    e.preventDefault();
+    var userInput = parseInt($("#number").val());
+    $("#result").text(beepBoop(userInput));
+  });
+
+});
