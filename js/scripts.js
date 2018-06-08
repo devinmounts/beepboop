ab4b73d9ea2ee28cbe57af56516b3c8076c271ff
Devin Mounts <devinmounts@gmail.com>
2018-06-08 14:47:58 -0700

    push i into empty array and replace numbers with programmed responses, errors exist.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 function beepboop(number){
   var response = [];
 
-  for(i=0; i<=number; i++)
+  for(i=0; i<=number; i++) {
     response.push(i);
-    console.log(response)
-  
-//     if (isNaN(number)) {
-//         response.push("not a number");
-//       } else if (number % 3 === 0) {
-//         response.push("I'm sorry Dave, I can't allow you to do that.")
-//       } else if (number === 1) {
-//         response.push("Boop")
-//       } else if (number === 0) {
-//           response.push("Beep")
-//       } else
-//         response.push(number)
-//
-//           console.log(response);
-//
-//     return response.join("");
+    if (isNaN(i)) {
+      response.push("not a number");
+    } else if (i % 3 === 0) {
+        response.push("I'm sorry Dave, I can't allow you to do that.")
+      } else if (i === 1) {
+        response.push("Boop")
+      } else if (i === 0) {
+          response.push("Beep")
+      } else
+        response.push(i)
+
+          console.log(response);
+
+  }
+    return response.join("");
 }
