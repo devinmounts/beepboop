ffdc977b4b95263d7a346ab7e277ac8bc58ce132
Devin Mounts <devinmounts@gmail.com>
2018-06-08 13:19:20 -0700

    create string of numbersgiven user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 function beepboop(number){
   var response = [];
-  var array = [number];
-  console.log(array);
-
-    if (isNaN(array)) {
-        response.push("not a number");
-      } else if (array % 3 === 0) {
-        response.push("I'm sorry Dave, I can't allow you to do that.")
-      } else if (array === 1) {
-        response.push("Boop")
-      } else if (array === 0) {
-          response.push("Beep")
-      } else
-        response.push(array)
-
-          console.log(array)
-
-    return response.join("");
+  var str = "";
+  var integers = parseInt(str);
+for (i=0; i<=number; i++){
+  str = str + i;
+  console.log(str)
+ }
+ return integers;
+    // if (isNaN(array)) {
+    //     response.push("not a number");
+    //   } else if (array % 3 === 0) {
+    //     response.push("I'm sorry Dave, I can't allow you to do that.")
+    //   } else if (array === 1) {
+    //     response.push("Boop")
+    //   } else if (array === 0) {
+    //       response.push("Beep")
+    //   } else
+    //     response.push(array)
+    //
+    //       console.log(array)
+    //
+    // return response.join("");
 }
