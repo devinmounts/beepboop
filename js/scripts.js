d8f35e4e2a7a49ed4a632b3660d000d662167425
Devin Mounts <devinmounts@gmail.com>
2018-06-08 14:38:36 -0700

    populate an array from zero to 'number'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
 function beepboop(number){
   var response = [];
-  var str = "";
-  var array = ;
 
-    for (i=0; i<=number; i++){
-      array = array + i;
-
-      console.log(array);
-     }
-     return array;
-
-    // if (isNaN(array)) {
-    //     response.push("not a number");
-    //   } else if (array % 3 === 0) {
-    //     response.push("I'm sorry Dave, I can't allow you to do that.")
-    //   } else if (array === 1) {
-    //     response.push("Boop")
-    //   } else if (array === 0) {
-    //       response.push("Beep")
-    //   } else
-    //     response.push(array)
-    //
-    //       console.log(array)
-    //
-    // return response.join("");
+  for(i=0; i<=number; i++)
+    response.push(i);
+    console.log(response)
+  
+//     if (isNaN(number)) {
+//         response.push("not a number");
+//       } else if (number % 3 === 0) {
+//         response.push("I'm sorry Dave, I can't allow you to do that.")
+//       } else if (number === 1) {
+//         response.push("Boop")
+//       } else if (number === 0) {
+//           response.push("Beep")
+//       } else
+//         response.push(number)
+//
+//           console.log(response);
+//
+//     return response.join("");
 }
