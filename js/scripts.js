b6714bd388165edf84ac159db878efa5d419cff4
Devin Mounts <devinmounts@gmail.com>
2018-06-10 18:32:58 -0700

    finish else if statements to return required responses.

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,26 @@
 function beepBoop(number){
   var array = [];
-  var intString = integer.toString();
 
     for (integer=0; integer<=number; integer++){
 
-          var intString = integer.toString();
-          if (intString.includes(1)){
-              array.push([intString])
-            }else
-            array.push("false");
-      // for (digit=0; digit<=[integer]; digit++){
-      // console.log(digit);
-      // array.push([digit.toString()])
-      // }
-      // forEach(array.includes(1)){
-      //   array.push(true);
-      // }
-
-
+      var intString = integer.toString();
+      if (intString % 3 ===0) {
+        array.push("I'm sorry Dave, I can't allow you to do that");
+        }else if (intString.includes(0)) {
+          array.push("Beep");
+        }else if (intString.includes(1)) {
+          array.push("Boop");
+        }else
+          array.push(intString);
     }
- return array;
+ return array.join(" ");
 }
 
+$(function(){
+  $("#beepboop").submit(function(e){
+    e.preventDefault();
+    var userInput = parseInt($("#number").val());
+    $("#result").text(beepBoop(userInput));
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   var response = [];
-//   // var number = input.split("");
-//
-//   for(integer=0; integer<=number; integer++) {
-//     var digits = (number[digits]);
-//     for(digits=0; digits<=number; digits++)
-//     console.log(number);
-//     console.log(digits);
-//     if (isNaN(digits)) {
-//       response.push("not a number");
-//     } else if (i % 3 === 0) {
-//         response.push("I'm sorry Dave, I can't allow you to do that.")
-//       } else if (i === 1) {
-//         response.push("Boop")
-//       } else if (i === 0) {
-//           response.push("Beep")
-//       } else
-//           response.push(i)
-//
-//           console.log(response);
-//   }
-//     return response.join(" ");
-// }
-
-// $(function(){
-//   $("#beepboop").submit(function(e){
-//     e.preventDefault();
-//     var userInput = parseInt($("#number").val());
-//     $("#result").text(beepBoop(userInput));
-//   });
-//
-// });
+});
